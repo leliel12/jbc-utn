10c8385fde85dc505cad59b9a8f32298b5b516e6
jbc.develop <jbc.develop@26ab6a7c-abb7-11dd-9cef-df7ddf1e6aee>
2009-03-26 05:23:51 +0000

    end sugerido 01

--- 5to/IA/sugerido_01/eq.py
+++ 5to/IA/sugerido_01/eq.py
@@ -18,8 +18,8 @@ def get_equalized_histogram(histogram):
     for value in histogram:
         last += levels * value
         new_idx = int(round(last))
-        if new_idx == levels:
-            new_idx -= 1
+        if new_idx >= levels:
+            new_idx = levels - 1 
         eq_histogram[new_idx] += value
     return eq_histogram 
        
@@ -33,11 +33,23 @@ def equalize(img, levels):
 if __name__ == '__main__':
     print("TEST RUN")
     
-    i0 = [10, 20, 30]
-    i1 = [23, 45, 35]
-    i2 = [54, 65, 28]
-    img = [i0, i1, i2]
+    img = [
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+        [2, 15, 2, 2, 12, 2, 2, 2, 10, 2],
+        [2, 15, 2, 12, 15, 12, 2, 9, 2, 2],
+        [2, 2, 12, 15, 15, 15, 12, 2, 2, 2],
+        [2, 12, 15, 15, 15, 15, 15, 12, 2, 2],
+        [2, 12, 15, 15, 15, 15, 15, 12, 2, 2],
+        [2, 12, 15, 15, 15, 15, 15, 12, 2, 2],
+        [2, 12, 12, 12, 12, 12, 12, 12, 2, 2],
+        [2, 2, 2, 2, 2, 2, 2, 2, 10, 2],
+        [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+        ]
     
-    levels = 255
+    levels = 16
     
-    print equalize(img, levels)
+    eq = equalize(img, levels)
+    i = 0
+    for value in eq:
+        print("Intensity %i: %f" % (i, value))
+        i += 1
